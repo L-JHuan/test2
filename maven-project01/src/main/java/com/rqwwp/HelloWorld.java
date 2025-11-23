f466e9e8745370866925482e7166db2d6cb7c087
LJH <1466356503@qq.com>
2025-11-23 20:06:34 +0800

    测试dev分支

--- maven-project01/src/main/java/com/rqwwp/HelloWorld.java
+++ maven-project01/src/main/java/com/rqwwp/HelloWorld.java
@@ -6,6 +6,6 @@ package com.rqwwp;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("第二次提交");
+        System.out.println("测试dev分支");
     }
 }
