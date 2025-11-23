bfc99b31ecd381b6c806446e00ed43418bd3f34e
LJH <1466356503@qq.com>
2025-11-23 20:36:35 +0800

    conflict resolved

--- maven-project01/src/main/java/com/rqwwp/HelloWorld.java
+++ maven-project01/src/main/java/com/rqwwp/HelloWorld.java
@@ -7,5 +7,7 @@ package com.rqwwp;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("测试dev分支");
+        System.out.println("第二次提交");
+        System.out.println("这是main分支");
     }
 }
