8ae25ac62400dfe230df1dd3b73ba840a37f40ec
LJH <1466356503@qq.com>
2025-11-23 19:00:25 +0800

    helloWorld

---
+++ maven-project01/src/main/java/com/rqwwp/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.rqwwp;
+
+/**
+ * @author LJH
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
