b68709a518d720defb8ce3bc78f280c02bf804ac
LJH <1466356503@qq.com>
2025-11-23 19:27:23 +0800

    第二次提交

--- maven-project01/src/main/java/com/rqwwp/HelloWorld.java
+++ maven-project01/src/main/java/com/rqwwp/HelloWorld.java
@@ -6,6 +6,6 @@ package com.rqwwp;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("第二次提交");
     }
 }
